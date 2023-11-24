f152b4fd8aae0c864a216d9c116b69bc3ffbeef8
Adrian Jose Diaz Guilamo <adrian96guilamo@gmail.com>
2023-11-24 18:37:59 -0400

    ngrx setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -9,6 +9,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { GenericModule } from './modules/shared/generic.module';
 import { MaterialModule } from './modules/material/material.module';
 import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
 @NgModule({
   declarations: [
@@ -21,7 +22,8 @@ import { StoreModule } from '@ngrx/store';
     HttpClientModule,
     GenericModule,
     MaterialModule,
-    StoreModule.forRoot({}, {})
+    StoreModule.forRoot({}, {}),
+    StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: !isDevMode() })
   ],
   providers: [],
   bootstrap: [AppComponent]
